775957302afdb4c518863c74c2ca39eb4c66ea17
nxjosephofficial <nxjosephofficial@protonmail.com>
2022-10-07 23:14:00 +0300

    Ask to user for filename and file content then read

--- file_handling/main.rb
+++ file_handling/main.rb
@@ -1,6 +1,13 @@
 #!/usr/bin/env ruby -wKU
-fileobject = File.new("sample.txt", "w+");
+puts "Enter file name"
+file_name = gets.chomp
+puts "Enter something to write into file"
+file_content = gets.chomp
+fileobject = File.new(file_name, "w+");
 
-fileobject.syswrite("File handling");
+fileobject.syswrite(file_content);
+puts
+puts "Content of file:"
+puts File.read(file_name)
 
 fileobject.close();
